30f8637c01b10e1e297231851d178e108e243478
SeRGiioxAG <sergioastudillo@protonmail.com>
2020-07-05 17:06:19 +0200

    Added a responsive button. v0.6.2

--- js/index.js
+++ js/index.js
@@ -9,17 +9,22 @@ let superSecretPassword = [];*/
 function checkPassword() {
     const fullPasswordPattern = /^(?=.*?[A-Z])(?=.*?[a-z])(?=.{3,}?[0-9])(?=.*?[#?!@$%^&*-]).{8,30}$/;
 
-    /*const totalLength = /.{8,30}/;
-    const minimumNumbers = /(?=.{3,}?[0-9])/;
+    const totalLength = /.{8,30}/;
+    const minimumNumbers = /(?=.{2,}?[0-9])/;
     const minimumUpperCaseCharacters = /(?=.*?[A-Z])/;
     const minimumLowerCaseCharacters = /(?=.*?[a-z])/;
-    const minimumSymbols = /(?=.*?[#?!@$%^&*-])/;*/
+    const minimumSymbols = /(?=.*?[#?!@$%^&*-])/;
 
     const superSecretPassword = document.querySelector('#inputPassword').value.replace(/\s/g, '');
     if (superSecretPassword.match(fullPasswordPattern)) {
-        alert('Valid password');
+        console.log('Valid password');
         document.getElementById('inputPassword').value = '';
+        //TODO: Make the web automatically select the password input when failed.
+        document.getElementById('inputPassword').focus();
     } else {
-        alert('Invalid password');
+        console.log('Invalid password');
     }
+    /*TODO: IR VALIDANDO POCO A POCO, ES DECIR: MIRAR SI HAY 3 NÚMEROS EN EL TOTAL 
+    DE POSICIONES Y SI LOS HAY (COMPROBAR CON MATCH) HACER QUE SE ILUMINE EL CIRCULITO
+    AZUL.*/
 }
