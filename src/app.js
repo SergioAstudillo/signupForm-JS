4c9216f87542d2b16689d800d915beb5ecb35ae9
SeRGiioxAG <sergioastudillo@protonmail.com>
2020-07-14 20:32:40 +0200

    Upgraded project structure + new npm packages for CRUD on MySQL

--- src/app.js
+++ src/app.js
@@ -2,21 +2,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const router = require('./routes.js');
+const router = require('./routes/routes.js');
+const morgan = require('morgan');
 
-//Server initial settings
-app.set('port', 8080);
-app.set('views', 'views');
+/* Server initial settings */
+app.set('port', process.env.PORT || 8080);
+app.set('views', 'src/views');
 app.set('view engine', 'pug');
 
 /* Middlewares */
+app.use(morgan('dev'));
+
+/* Global variables */
 
 /* Routes */
 app.use(router);
 
 /* Static files */
-app.use('/assets', express.static('assets'));
-app.use('/public', express.static('public'));
+app.use('/assets', express.static('src/assets'));
+app.use('/public', express.static('src/public'));
 
 /* Server listening */
 app.listen(app.get('port'), () => {
