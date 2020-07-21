514a58630e1856b2f8b7ce66cb5c2d7ef2d20872
SeRGiioxAG <sergioastudillo@protonmail.com>
2020-07-21 21:03:53 +0200

    Display data from DB TEST.

--- src/routes/authentication.js
+++ src/routes/authentication.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const router = express.Router();
+/* Test */
+const pug = require('pug');
+const path = require('path');
 
 //Import the connection created in database.js
 const dbCONNECTION = require('../database');
 const pool = require('../database');
 
-//
+/* Receive the form in POST and insert the data in the DB. */
 router.post('/signup', async (req, res) => {
     const { emailInput, passwordInput } = req.body;
 
@@ -18,14 +21,29 @@ router.post('/signup', async (req, res) => {
         fullname,
         password,
     };
-
-    console.log(newUser);
     try {
         await pool.query('INSERT INTO users set ?', [newUser]);
     } catch (err) {
         console.log(err);
+        console.log('There has been an error inserting the data in the DB.');
     }
     console.log('Data received');
+});
+
+/* Retrieve the data from the DB when the user is logged in. */
+router.get('/profile', async (req, res) => {
+    try {
+        //TODO: make this request only select the account that the user introduced in the login page.
+        const users = await pool.query(`SELECT * FROM users;`);
+        res.render('profile', {
+            email: users[0].email,
+            fullname: users[0].fullname,
+            password: users[0].password,
+            created_at: users[0].created_at,
+        });
+    } catch (err) {
+        console.log(`There has been an error trying to select the user from the database.\nOR \nThere has been an error trying to render /profile page.\nThis is the error code: \n${err}`);
+    }
 });
 
 //Export router module.
